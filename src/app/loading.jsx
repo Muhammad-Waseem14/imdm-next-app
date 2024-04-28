9439a070c828e5f4a877065b116ebdcdb0e07b1d
Muhammad-Waseem14 <muhammad.wasim@arbisoft.com>
2024-04-28 21:01:35 +0500

    add loading spinner

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function loading() {
+  return (
+    <div className="flex justify-center mt-16">
+      <div className="h-52">Loading...</div>
+    </div>
+  );
+}
+
+export default loading;
